b22038a0ca8c4cadef544fcf00542b2421b75e40
unknown <williambrooks287@gmail.com>
2021-12-17 16:27:36 -0500

    define arrays

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Define Arrays
+var upperStr = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
+var upper = upperStr.split(" ");
+var lowerStr = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
+var lower = lowerStr.split(" ");
+var numberStr = "1 2 3 4 5 6 7 8 9 0";
+var number = numberStr.split(" ");
+var specialStr = "! # $ % & ' ( ) * + , - . / : ; < = > ? @ [ ] / ^ _ ` { | } ~";
+var special = specialStr.split(" ");
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
